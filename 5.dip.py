f97ba36bd33f7e90123558749669506a63774c4d
Gugirotto34 <112582904+Gugirotto34@users.noreply.github.com>
2023-04-28 10:59:08 -0300

    correção com base no 5º principio SOLID

--- 5.dip.py
+++ 5.dip.py
@@ -4,9 +4,23 @@ Dependency Inversion Principle
 Dependências devem ser feitas sobre abstrações, não sobre implementações concretas 
 
 """
+from abc import ABC, abstractmethod
 
+class Entity(ABC):
+    def __init__(self,name,hp,speed):
+        self.__name = name
+        self.__hp = hp
+        self.__speed = speed
+    
+    @abstractmethod
+    def hp(self):
+        pass
 
-class Player:
+    @abstractmethod
+    def name(self):
+        pass
+
+class Player(Entity):
     def __init__(self, name):
         self.stats = StatsReporter(self)
         self.__name = name
@@ -20,7 +34,7 @@ class Player:
         return self.__name
 
 class StatsReporter:
-    def __init__(self, char: Player):
+    def __init__(self, char: Entity):
         self.char = char
 
     def report(self):
